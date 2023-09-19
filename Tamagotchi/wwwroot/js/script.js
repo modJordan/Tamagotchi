e90d4202654f8db84cd94e6b3c691f7d08b95e91
modjordan <mod.jordan@icloud.com>
2023-09-18 22:54:58 -0700

    working on js animation for sprite WIP.

--- Tamagotchi/wwwroot/js/script.js
+++ Tamagotchi/wwwroot/js/script.js
@@ -1,9 +1,35 @@
 let frameIndex = 0;
-const totalFrames = 4; // The total number of frames in your sprite sheet
+const totalFrames = 14; // The total number of frames in your sprite sheet
+const frameHeight = 15;
 
-setInterval(function() {
-  const xPosition = -(frameIndex * 15); // 100 is the width of one frame
-  document.getElementById('sprite').style.backgroundPosition = xPosition + 'px 0';
+// Function to update the sprite frame
+function updateSprite() {
+  console.log('updateSprite called')
+  // Calculate the background-position
+  const newPosition = -(frameIndex * frameHeight) + 'px';
+  console.log(newPosition);
 
-  frameIndex = (frameIndex + 1) % totalFrames; // Reset after the last frame
-}, 100); // 100 milliseconds for each frame
+
+  // Update the background-position
+  document.getElementById("sprite").style.backgroundPosition = `0px ${newPosition}`;
+
+  // Increment the frame index
+  frameIndex = (frameIndex + 1) % totalFrames;
+}
+
+// Set an interval to update the sprite frame
+// setInterval(updateSprite, 500); // Updates every 250 milliseconds
+
+window.addEventListener("load", function () {
+  console.log("working");
+  setInterval(updateSprite, 500);
+  updateSprite();
+});
+
+
+// setInterval(function () {
+//   const xPosition = -(frameIndex * 15); // 100 is the width of one frame
+//   document.getElementById('sprite').style.backgroundPosition = xPosition + 'px 0';
+
+//   frameIndex = (frameIndex + 1) % totalFrames; // Reset after the last frame
+// }, 100); // 100 milliseconds for each frame
